51f82e52238f80b736eaac5f8d058a6ee00ee569
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-07-25 22:09:56 -0300

    last update

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,12 @@
+
+
 const Login = () => {
     return (
-    <div>Login</div>
+    <div className="flex min-h-screen">
+        <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+            
+        </div>
+    </div>
     )
 }
 
---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,6 @@
+
+
+export async function GET(request: Request) {
+
+    console.log('teste')
+}
