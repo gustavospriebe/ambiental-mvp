d7048067768515396abeb8440fb4c87885df140b
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-08-11 16:00:28 -0300

    create new certification working

--- app/api/certifications/route.ts
+++ app/api/certifications/route.ts
@@ -1,15 +1,11 @@
 import { db } from "@/lib/db";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "../auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 import { Certification } from "@prisma/client";
 
 // Puxar as infos gerais das certificações e ultimas tasks
-export async function GET(req: Request, res: NextResponse) {
-  // // // Teste chamada direto api
-  // const session = await getServerSession(authOptions);
-  // const sessionId = session?.user.id;
-
+export async function GET(req: NextRequest, res: NextResponse) {
   const sessionId = req.headers.get("session-id");
 
   if (!sessionId) {
@@ -35,13 +31,8 @@ export async function GET(req: Request, res: NextResponse) {
 }
 
 // Criar nova certificação
-// const session = await getServerSession(authOptions);
-// const sessionId = session?.user.id;
-// Teste chamada direto api
 export async function POST(req: Request, res: Response) {
   const sessionId = req.headers.get("session-id");
-
-console.log(sessionId);
 
   if (!sessionId) {
     return new NextResponse(JSON.stringify({ error: "Unauthorized" }), {
@@ -49,39 +40,23 @@ console.log(sessionId);
     });
   }
 
-  // const name = req.headers.get("name");
-  // const description = req.headers.get("description");
-  // const due = req.headers.get("due");
+  const { name, description, due } = await req.json();
 
-  // if (!name || !description || !due) {
-  //   return new NextResponse(
-  //     JSON.stringify({ error: "Invalid request headers" }),
-  //     {
-  //       status: 400,
-  //     },
-  //   );
-  // }
-
-  const body = req.body as {
-    name: string;
-    description: string;
-    due: string;
-  } | null;
-
-  console.log(body);
-
-  if (!body) {
-    return new NextResponse(JSON.stringify({ error: "Invalid request body" }), {
-      status: 400,
-    });
+  if (!name || !description || !due) {
+    return new NextResponse(
+      JSON.stringify({ error: "Invalid request headers" }),
+      {
+        status: 400,
+      },
+    );
   }
 
   const newData = await db.certification.create({
     data: {
-      name: body.name,
+      name: name,
       companyId: sessionId,
-      description: body.description,
-      due: body.due,
+      description: description,
+      due: due,
     },
   });
 
--- components/ButtonNewTask.tsx
+++ components/ButtonNewTask.tsx
@@ -25,52 +25,37 @@ interface ButtonNewTask extends React.HTMLAttributes<HTMLElement> {
 //   due: z.date(),
 // });
 
-const ButtonNewTask = ({ children, className }: ButtonNewTask) => {
+const ButtonNewTask = ({ children, className, sessionId }: ButtonNewTask) => {
   const router = useRouter();
-
-  const session = useSession();
-
-  if (session.status !== "authenticated") return;
-
-  const sessionId = session.data.user.id;
-
-  console.log(sessionId);
 
   async function newCertification() {
     try {
-      const req = await axios.post("http://localhost:3000/api/certifications", {
+      let data = {
+        name: "cris",
+        description: "OIOIOIOII",
+        due: "2023-11-17T03:00:00.000Z",
+      };
+
+      let config = {
+        method: "post",
+        maxBodyLength: Infinity,
+        url: "http://localhost:3000/api/certifications",
         headers: {
-          "session-id": "a36c1d4c-8edc-409a-ab3f-285081a00e7a",
+          "session-id": sessionId,
           "Content-Type": "application/json",
         },
-        body: {
-          name: "Fachjuujihksenok Certification",
-          description: "Lorem ipsum fkfj",
-          due: "2023-10-27T03:00:00.000Z",
-        },
-      });
+        data: data,
+      };
 
-      // redirect("/certifications")
+      const req = await axios.request(config);
+
+      console.log(req);
+
+      router.refresh();
       return req;
-      //     const res = await fetch("http://localhost:3000/api/certifications", {
-      //       method: "post",
-      //       headers: {
-      //         "session-id": sessionId,
-      //         "Content-Type": "application/json",
-      //       },
-      //       body: JSON.stringify({
-      //         name: "asdFaacenok Certification",
-      //         description: "Lorem ipsum fkfj",
-      //         due: "2023-10-27T03:00:00.000Z",
-      //       }),
-      //     });
-      //     console.log(res);
-
-      //     return res;
     } catch (error) {
       console.error("Erro no envio da requisição:", error);
     }
-    router.refresh();
   }
 
   return (
